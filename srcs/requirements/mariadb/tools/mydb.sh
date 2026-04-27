2a523ac5db72704b55626c0b6303c3b52884263c
svetameanssun <svetameanssun@gmail.com>
2026-04-27 21:07:58 +0200

    Inception in progress

---
+++ srcs/requirements/mariadb/tools/mydb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mysql_safe &
+
+sleep 5
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+
+mysqladmin shutdown
+
+mysql_safe
+
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,1 +1,45 @@
-#wp.sh automatiza  la intalaci'on de WordPress
+#!/bin/bash
+
+# Wait for MariaDB to be ready
+echo "Waiting MariaDB..."
+while ! mariadb -h mariadb -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SELECT 1" > /dev/null 2>&1; do
+    sleep 2
+done
+echo "MariaDB is ready!"
+
+cd /var/www/html/wordpress
+
+# Download WordPress if it doesn't exist
+if [ ! -f wp-config.php ]; then
+    echo "Downloading WordPress..."
+    wp core download --allow-root
+    
+    echo "Configuring WordPress..."
+    wp config create \
+        --dbname=${MYSQL_DATABASE} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=mariadb \
+        --allow-root
+
+    wp core install \
+        --url=https://${DOMAIN_NAME} \
+        --title="${WP_TITLE}" \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --allow-root
+
+    # Create second user (not admin)
+    wp user create ${WP_USER} ${WP_USER_EMAIL} \
+        --role=author \
+        --user_pass=${WP_USER_PASSWORD} \
+        --allow-root
+    
+    echo "WordPress is ready!"
+else
+    echo "WordPress is already configured."
+fi
+
+# Run PHP-FPM in the foreground
+php-fpm8.2 -F
